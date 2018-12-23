ef493a9cc41c4beeca67c16823ea052fe2d2488e
os4fun <guohui.xiao@gmail.com>
2018-12-23 14:25:51 -0600

    third commit

--- src/main/kotlin/OracleDslTest.kt
+++ src/main/kotlin/OracleDslTest.kt
@@ -13,7 +13,51 @@ object Regions : Table() {
     val name = varchar("region_name", 50) // Column<String>
 }
 
+class Greeter(val greeting: String) {
+    // call instance of objects as if they are function
+    operator fun invoke(name: String) {
+        println("$greeting, $name")
+    }
+}
+
+data class Issue (
+    val id: String, val project: String, val type:String, val priority: String, val description: String
+)
+
+class ImportantIssuesPredicate(val project: String)
+    : (Issue) -> Boolean {
+    override fun invoke(issue: Issue): Boolean {
+        return issue.project == project && issue.isImportant()
+    }
+
+    private fun Issue.isImportant(): Boolean {
+        return type == "Bug" && (priority == "Major" || priority == "Critical")
+    }
+}
+
+class DependencyHandler {
+    fun compile(coordination: String) {
+        println("Added dependencies on $coordination")
+    }
+
+    operator fun invoke(body: DependencyHandler.() -> Unit  ) {
+        body()
+    }
+}
+
+
 fun main(args: Array<String>) {
+    val bavarianGreeter = Greeter("Servus")
+    bavarianGreeter("Dmitry")
+
+   val i1 = Issue("IDEA-154446", "IDEA", "Bug", "Major", "Save settings failed")
+   val i2 = Issue("KT-12183", "Kotlin", "Feature", "Normal", "Intention: convert")
+
+    val predicate = ImportantIssuesPredicate("IDEA")
+    for (issue in listOf(i1, i2).filter(predicate)) {
+        println(issue.id)
+    }
+
    // Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
     Database.connect("jdbc:oracle:thin:@localhost:1521:orcl", driver = "oracle.jdbc.driver.OracleDriver", user = "HR", password = "hr4oracle")
 
